351f05c29005742210244e74210138a6db05c27b
Vinicius Teruel <vpteruel@gmail.com>
2024-03-02 18:47:33 -0500

    fix nav

--- public/includes/global-nav.php
+++ public/includes/global-nav.php
@@ -7,13 +7,13 @@
 
 <nav>
 	<ul>
-		<li><a href="<?php $urlPath ?>/index.php" title="Go to the Home page">Home</a></li>
-		<li><a href="<?php $urlPath ?>/red.php" title="Learn about red blocks">Red Blocks</a></li>
-		<li><a href="<?php $urlPath ?>/blue.php" title="Learn about blue blocks">Blue Blocks</a></li>
-		<li><a href="<?php $urlPath ?>/yellow.php" title="Learn about yellow blocks">Yellow Blocks</a></li>
-		<li><a href="<?php $urlPath ?>/green.php" title="Learn about green blocks">Green Blocks</a></li>
-		<li><a href="<?php $urlPath ?>/purple.php" title="Learn about purple blocks">Purple Blocks</a></li>
-		<li><a href="<?php $urlPath ?>/about.php" title="Learn more about us">About</a></li>
+		<li><a href="/public/index.php" title="Go to the Home page">Home</a></li>
+		<li><a href="/public/red.php" title="Learn about red blocks">Red Blocks</a></li>
+		<li><a href="/public/blue.php" title="Learn about blue blocks">Blue Blocks</a></li>
+		<li><a href="/public/yellow.php" title="Learn about yellow blocks">Yellow Blocks</a></li>
+		<li><a href="/public/green.php" title="Learn about green blocks">Green Blocks</a></li>
+		<li><a href="/public/purple.php" title="Learn about purple blocks">Purple Blocks</a></li>
+		<li><a href="/public/about.php" title="Learn more about us">About</a></li>
 	</ul>
 </nav>
 
