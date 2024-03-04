96b4abe8f3b4700c859cd2a31947d58c6d4be526
Vinicius Teruel <vpteruel@gmail.com>
2024-03-04 13:55:25 -0500

    Update green.php

--- public/green.php
+++ public/green.php
@@ -35,9 +35,9 @@
 		<!-- Page-level footer -->
 		<footer>
 			<!-- Fine print navigation -->
-			<nav>
-				<a href="about/privacy-policy.php" title="Read our Privacy Policy">Privacy Policy</a> | <a href="about/privacy-policy.php#personal-information" title="Learn about how we treat your personal information">Use of Personal Information</a> | <a href="about/terms-of-use.php" title="Understand our Terms of Use">Terms of Use</a> | <a href="about/terms-of-use.php#limitation-liability" title="Our Limitation of Liability explained">Limitation of Liability</a>
-			</nav>
+			<?php
+	                    include("includes/footer-nav.php");
+                        ?>
 			<p><small>© HTML, CSS, and JS Blocks Inc.</small></p>
 		</footer>
 	</body>
