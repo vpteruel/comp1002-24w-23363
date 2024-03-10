d4096d45b0be432488ce2cf1765cdbd57a37e9a6
Vinicius Teruel <vpteruel@gmail.com>
2024-03-10 16:25:13 -0400

    lab8 completed

---
+++ public/red.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>Red Blocks | COMP1002 HTML, CSS, and JS Fundamentals</title>
+		<meta name="author" content="The name of the document’s author to go here">
+		<meta name="description" content="A concise and accurate summary of the document content is to appear here">
+		<link rel="shortcut icon" href="images/favicon.png" type="image/x-icon" />
+		<link rel="stylesheet" href="css/styles.css" />
+	</head>
+	<body id="section2">
+		<!-- Page-level header -->
+		<?php
+			include('includes/header.php');
+		?>
+		<!-- Page-level main content -->
+		<main>
+			<section>
+				<h3>Red Blocks</h3>
+				<p>Viverra orci sagittis eu volutpat odio facilisis mauris sit amet. Lacus laoreet non curabitur gravida arcu ac tortor dignissim convallis. Ut enim blandit volutpat maecenas. Mauris commodo quis imperdiet massa. Sit amet dictum sit amet justo donec enim diam vulputate. Imperdiet nulla malesuada pellentesque elit.</p>
+				<p>Et malesuada fames ac turpis egestas integer eget aliquet nibh. Commodo quis imperdiet massa tincidunt nunc pulvinar. Odio pellentesque diam volutpat commodo sed. Enim eu turpis egestas pretium aenean. Faucibus purus in massa tempor nec. Duis tristique sollicitudin nibh sit amet. Tortor condimentum lacinia quis vel eros donec ac.</p>
+			</section>
+			<aside>
+				<figure>
+					<img src="images/red-block.png" width="350" height="237" alt="Red block" />
+					<figcaption><a href="https://openclipart.org/detail/224274/clip-is-a-brick-2x4" title="View the original artwork" rel="external">Brick</a> by Vanja (Public Domain)</figcaption>
+				</figure>
+			</aside>
+		</main>
+		<!-- Page-level footer -->
+		<?php
+			include('includes/footer.php');
+		?>
+	</body>
+</html>
