e139149aeae77616c6daa2254342f84be3f73b21
Vinicius Teruel <vpteruel@gmail.com>
2024-03-10 17:25:07 -0400

    logo path fixed

--- public/includes/header.php
+++ public/includes/header.php
@@ -1,5 +1,5 @@
 <header>
-	<img src="../images/html-logo.svg" height="75" width="75" alt="HTML" />
+	<img src="images/html-logo.svg" height="75" width="75" alt="HTML" />
 	<h1>HTML, CSS, and JS Fundamentals</h1>
 	<h2>Building Blocks for Web Developers</h2>
 	<?php
