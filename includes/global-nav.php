290ad6b5d5af5e6d6ac0b40cfea05a92935e857b
Vinicius Teruel <vpteruel@gmail.com>
2024-03-24 16:53:16 -0400

    finished

---
+++ includes/global-nav.php
@@ -0,0 +1,12 @@
+<!-- Global site navigation -->
+<nav>
+	<ul>
+		<li><a href="/index.php" title="Go to the Home page">Home</a></li>
+		<li><a href="/red.php" title="Learn about red blocks">Red Blocks</a></li>
+		<li><a href="/blue.php" title="Learn about blue blocks">Blue Blocks</a></li>
+		<li><a href="/yellow.php" title="Learn about yellow blocks">Yellow Blocks</a></li>
+		<li><a href="/green.php" title="Learn about green blocks">Green Blocks</a></li>
+		<li><a href="/purple.php" title="Learn about purple blocks">Purple Blocks</a></li>
+		<li><a href="/about.php" title="Learn more about us">About</a></li>
+	</ul>
+</nav>
