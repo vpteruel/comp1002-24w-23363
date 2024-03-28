0c25183df28036318b9e254374a23dc846767aec
Vinicius Teruel <vpteruel@gmail.com>
2024-03-27 20:21:51 -0400

    finish lab

---
+++ includes/global-nav.php
@@ -0,0 +1,17 @@
+<!-- Global site navigation -->
+<nav>
+	<ul>
+		<li><a href="/index.php" title="Go to the Home page">Home</a></li>
+		<li><a href="/red.php" title="Learn about red blocks">Red Blocks</a></li>
+		<li><a href="/blue.php" title="Learn about blue blocks">Blue Blocks</a></li>
+		<li><a href="/yellow.php" title="Learn about yellow blocks">Yellow Blocks</a></li>
+		<li><a href="/green.php" title="Learn about green blocks">Green Blocks</a></li>
+		<li><a href="/purple.php" title="Learn about purple blocks">Purple Blocks</a></li>
+		<li><a href="/about.php" title="Learn more about us">About</a>
+			<ul>
+				<li><a href="/about/privacy-policy.php" title="Learn about our privacy policy">Privacy Policy</a></li>
+				<li><a href="/about/terms-of-use.php" title="Learn about our terms of use">Term of Use</a></li>
+			</ul>
+		</li>
+	</ul>
+</nav>
